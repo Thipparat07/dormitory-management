b7e9403c5fcd94249b234e8d5a15ff08c1449567
Toey <142540448+Thipparat07@users.noreply.github.com>
2026-09-02 00:09:11 +0700

    Update production API endpoint URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiEndpoint: 'https://api.dormmy.online' // Placeholder for real production URL
+    apiEndpoint: 'https://dormmy.baantable.uk' // Placeholder for real production URL
 };
